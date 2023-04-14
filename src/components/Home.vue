c8b55221c9716e1f7756005c518b52c7a8f2de9c
kernel32dev <kernel32dev@gmail.com>
2023-04-14 15:07:27 -0300

    adicionado frontend em vue.js

---
+++ src/components/Home.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="home">
+        Teste de overflow vertical:<br>
+        Linha #1<br>
+        Linha #2<br>
+        Linha #3<br>
+        Linha #4<br>
+        Linha #5<br>
+        Linha #6<br>
+        Linha #7<br>
+        Linha #8<br>
+        Linha #9<br>
+        Linha #10<br>
+        Linha #11<br>
+        Linha #12<br>
+        Linha #13<br>
+        Linha #14<br>
+        Linha #15<br>
+        Linha #16<br>
+        Linha #17<br>
+        Linha #18<br>
+        Linha #19<br>
+        Linha #20<br>
+        Linha #21<br>
+        Linha #22<br>
+        Linha #23<br>
+        Linha #24<br>
+        Linha #25<br>
+        Linha #26<br>
+        Linha #27<br>
+        Linha #28<br>
+        Linha #29<br>
+        Linha #30<br>
+        Linha #31<br>
+        Linha #32<br>
+        Linha #33<br>
+        Linha #34<br>
+        Linha #35<br>
+        Linha #36<br>
+        Linha #37<br>
+        Linha #38<br>
+        Linha #39<br>
+        Linha #40<br>
+        Linha #41<br>
+        Linha #42<br>
+        Linha #43<br>
+        Linha #44<br>
+        Linha #45<br>
+        Linha #46<br>
+        Linha #47<br>
+        Linha #48<br>
+        Linha #49<br>
+        Linha #50<br>
+        Linha #51<br>
+        Linha #52<br>
+        Linha #53<br>
+        Linha #54<br>
+        Linha #55<br>
+        Linha #56<br>
+        Linha #57<br>
+        Linha #58<br>
+        Linha #59<br>
+        Linha #60<br>
+        Linha #61<br>
+        Linha #62<br>
+        Linha #63<br>
+        Linha #64<br>
+        Linha #65<br>
+        Linha #66<br>
+        Linha #67<br>
+        Linha #68<br>
+        Linha #69<br>
+        Linha #70<br>
+        Linha #71<br>
+        Linha #72<br>
+        Linha #73<br>
+        Linha #74<br>
+        Linha #75<br>
+        Linha #76<br>
+        Linha #77<br>
+        Linha #78<br>
+        Linha #79<br>
+        Linha #80<br>
+        Linha #81<br>
+        Linha #82<br>
+        Linha #83<br>
+        Linha #84<br>
+        Linha #85<br>
+        Linha #86<br>
+        Linha #87<br>
+        Linha #88<br>
+        Linha #89<br>
+        Linha #90<br>
+        Linha #91<br>
+        Linha #92<br>
+        Linha #93<br>
+        Linha #94<br>
+        Linha #95<br>
+        Linha #96<br>
+        Linha #97<br>
+        Linha #98<br>
+        Linha #99<br>
+        Linha #100<br>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+    text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+
+import App from '@/App.vue'
+
+import router from '@/router'
+
+Vue.config.productionTip = false
+
+// Vue.use(VueRouter)
+
+import '@/assets/style.css'
+import '@/assets/fontawesome/fontawesome.css'
+import '@/assets/fontawesome/solid.css'
+
+const vue = new Vue({
+    router,
+    render: h => h(App)
+})
+
+if (/(android|webos|iphone|ipad|ipod|blackberry|windows phone|tablet|ipad)/.test(navigator.userAgent.toLowerCase())) {
+    document.body.classList.add('mobile')
+} else {
+    document.body.classList.add('desktop')
+}
+
+vue.$mount('#app')
